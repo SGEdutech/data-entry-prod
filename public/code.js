ae9d3fe558c366a5edfd863be948abd05b438dd3
Ashish Kuamr <entropy.ash@gmail.com>
2018-06-18 13:00:53 +0530

    Ajax request for form and modals added

--- public/code.js
+++ public/code.js
@@ -1,3 +1,21 @@
 $('select').formSelect();
 $('.timepicker').timepicker();
 $('.datepicker').datepicker();
+$('.modal').modal();
+
+const elem = document.querySelector('.modal');
+const instance = M.Modal.getInstance(elem);
+const form = $('#the_form');
+
+form.submit(e => {
+    e.preventDefault();
+    $.ajax({
+        type: form.attr('method'),
+        url: form.attr('action'),
+        data: form.serialize(),
+        success: data => {
+            instance.open();
+        },
+        error: err => console.error(err)
+    })
+});
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
 const PORT = require('./config').SERVER.PORT;
 const storageEngion = require('./storage-engine');
 require('./database/connection');
@@ -14,7 +15,9 @@ const routes = {
 
 const app = express();
 
-app.get('/', (req, res) => res.redirect('./app/maintenance.html'))
+app.use(cors());
+
+app.get('/', (req, res) => res.redirect('./app/maintenance.html'));
 
 app.use('/app', express.static(path.join(__dirname, 'public')));
 
@@ -34,16 +37,16 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
 app.use('/blog', routes.blog);
-app.use('/event', storageEngion.eventCoverPicMiddleware, filterEmptyObjects, routes.event);
-app.use('/school', storageEngion.schoolCoverPicMiddleware, filterEmptyObjects, routes.school);
-app.use('/tuition', storageEngion.tuitionCoverPicMiddleware, filterEmptyObjects, routes.tuition);
+app.use('/event', storageEngion.eventCoverPicMiddleware, filterEmptyReqObjects, routes.event);
+app.use('/school', storageEngion.schoolCoverPicMiddleware, filterEmptyReqObjects, routes.school);
+app.use('/tuition', storageEngion.tuitionCoverPicMiddleware, filterEmptyReqObjects, routes.tuition);
 app.use('/user', routes.user);
 
 app.listen(PORT, () => {
     console.log(`Yo dawg! Server's at http://localhost:${PORT}`);
 });
 
-function filterEmptyObjects(req, res, next) {
+function filterEmptyReqObjects(req, res, next) {
     Object.keys(req.body).forEach(element => {
         if (req.body[element] === '') {
             delete req.body[element];
