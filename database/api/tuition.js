4a23d0b88931d36f6a15d94c014a8c1545ae9232
Ashish Kuamr <entropy.ash@gmail.com>
2018-06-20 17:51:01 +0530

    Fix: delete tuition

--- database/api/tuition.js
+++ database/api/tuition.js
@@ -24,17 +24,17 @@ route.post('/', (req, res) => {
     tuitionDbFunctions.addCollection(req.body).then(data => res.send(data)).catch(err => console.error(err));
 });
 
-route.put('/:userId', (req, res) => {
+route.put('/:_id', (req, res) => {
     tuitionDbFunctions.updateOneRow(req.params, req.body).then(data => res.send(data)).catch(err => console.error(err));
 });
 
 route.delete('/delete/:arrayName/:_id', (req, res) => {
-    schoolDbFunctions.deleteElementFromArray({_id: req.params._id}, req.params.arrayName, req.body)
+    tuitionDbFunctions.deleteElementFromArray({_id: req.params._id}, req.params.arrayName, req.body)
         .then(data => res.send(data))
         .catch(err => console.error(err));
 });
 
-route.delete('/:userId', (req, res) => {
+route.delete('/:_id', (req, res) => {
     tuitionDbFunctions.deleteOneRow(req.params).then(data => res.send(data)).catch(err => console.error(err));
 });
 
